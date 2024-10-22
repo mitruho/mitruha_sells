9fb71ede0923e9dfe82df0c854a939c0cd1b928d
mitruhaa <charonpersonal@proton.me>
2024-10-22 17:54:28 +0200

    3243232

--- core/views.py
+++ core/views.py
@@ -3,12 +3,30 @@ from .models import Item, AboutPage
 from django.core.paginator import Paginator
 from django.views.generic.detail import DetailView
 
+def rearrange_items(items):
+    n = len(items)
+    center = (n - 1) // 2
+    result = []
+    for i in range(n):
+        if i % 2 == 0:
+            index = center + (i // 2)
+        else:
+            index = center - ((i + 1) // 2)
+        result.append(items[index])
+    return result
+
+
 def index(request):
-    page_number = request.GET.get('page', 1)
-    paginator = Paginator(Item.objects.all().order_by('created_at'), 3)
+    # Fetch your items as usual
+    items = Item.objects.all()
+    # Rearrange items
+    rearranged_items = rearrange_items(list(items))
+    # Paginate if necessary
+    paginator = Paginator(rearranged_items, per_page=10)
+    page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    context = {'page_obj': page_obj}
-    return render(request, 'core/index.html', context=context)
+    return render(request, 'core/index.html', {'page_obj': page_obj})
+
 
 def about_view(request):
     about_content = AboutPage.objects.first()  # assumes there is at least one entry
